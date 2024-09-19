d84e35bd90abfa9559e9738235abce467117418c
danildanildanil3 <161597560+danildanildanil3@users.noreply.github.com>
2024-09-19 15:02:24 +0400

    Add files via upload

---
+++ TextModifier.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class TextModifier {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите текст: ");
+        String Text = scanner.nextLine();
+        int sum = 0;
+
+        // Алгоритм изменения текста
+        StringBuilder modifiedText = new StringBuilder();
+        for (int i = 0; i < Text.length()-1; i++) {
+            char currentChar = Text.charAt(i);
+            boolean flag = true;
+
+            if (Text.charAt(i) == ' ' && i < Text.length() - 1 && Text.charAt(i + 1) != ' ') {
+                modifiedText.append(" ");
+                flag = false;
+
+
+            }
+
+            if (Text.charAt(i + 1) == '-' && i < Text.length() - 2) {
+                modifiedText.append(Text.charAt(i + 2));
+                modifiedText.append(Text.charAt(i));
+                flag = false;
+                i += 2;
+
+            }
+
+
+            if (Text.charAt(i) == '+') {
+                modifiedText.append('!');
+                flag = false;
+
+
+            }
+
+            if (Character.isDigit(Text.charAt(i))) {
+                sum += Character.getNumericValue(Text.charAt(i));
+                flag = false;
+                if (flag) {
+                    modifiedText.append(Text.charAt(i));
+
+                }
+
+
+
+            }
+
+        }
+        modifiedText.append(sum);
+
+
+        System.out.println("Измененный текст: " + modifiedText);
+    }
+}
+
